35d6292b0ff651864a6b003c1526287ec43c94c6
testyourmine <25854988+testyourmine@users.noreply.github.com>
2025-04-30 04:04:28 -0500

    Decomp InitializeGame and fix linker

--- multiboot/include/variables.h
+++ multiboot/include/variables.h
@@ -2,6 +2,7 @@
 #define VARIABLES_H
 
 #include "types.h"
+#include "structs/structs.h"
 
 extern u8 gMainGameMode; // 0x03000010
 extern u16 gUnk_24; // 0x03000024
@@ -9,13 +10,17 @@ extern s8 gUnk_6c; // 0x0300006c
 extern u8 gUnk_70; // 0x03000070
 extern u16 gUnk_78; // 0x03000078
 extern u8 gUnk_7c; // 0x0300007c
+extern u16 gUnk_80; // 0x03000080
 extern u8 gUnk_88; // 0x03000088
 extern u16 gUnk_8c; // 0x0300008c
 extern u8 gUnk_290; // 0x03000290
 extern u8 gFrameCounter8Bit; // 0x030006a0
 extern u8 gUnk_6a8; // 0x030006a8
 extern u16 gUnk_6ac; // 0x030006ac
+extern u16 gUnk_6b8; // 0x030006b8
 extern u8 gUnk_6bc; // 0x030006bc
 extern u16 gUnk_6c0; // 0x030006c0
+
+extern u16 (*gIntrCodePointer)[0x100]; // 0x0x03007ffc
 
 #endif /* VAIRABLES_H */
--- multiboot/src/main.c
+++ multiboot/src/main.c
@@ -128,3 +128,46 @@ u32 LoadGraphics(void)
     }
     return loadedGraphics;
 }
+
+void InitializeGame(void)
+{
+    u8 tmp;
+    u8 tmp2;
+    u8 tmp3;
+    u16 tmp4;
+    
+    dma_fill32(3, 0, IWRAM_BASE, IWRAM_SIZE - 0x15D);
+    write16(REG_WAITCNT, WAIT_GAMEPACK_CGB | WAIT_BANK0_SUBSEQUENT_1CYCLE | WAIT_BANK0_3CYCLES);
+    DMA_SET(3, InterruptTable, 0x3000030, 0x8400000e);
+    DMA_SET(3, 0x2000104, 0x3000090, 0x84000080);
+    gIntrCodePointer = (void*)0x3000090;
+    dma_fill16(3, 0, VRAM_BASE, VRAM_SIZE);
+    DMA_SET(3, ARRAY_02001854, PALRAM_OBJ, 0x80000100);
+    ApplyMonochromeToPalette((u16*)ARRAY_02001854, PALRAM_BASE, 0);
+    ApplyMonochromeToPalette((u16*)ARRAY_02001854, VRAM_BASE + 0x1000, -8);
+    LZ77UncompVRAM(ARRAY_02001a54, ((tmp2 = UNK_7E44.v8) << 14) + VRAM_BASE);
+    LZ77UncompVRAM(ARRAY_02005550, ((tmp = UNK_7E44.v9) << 11) + VRAM_BASE);
+    LZ77UncompVRAM(ARRAY_02005b10, ((tmp3 = UNK_7E44.v1) << 11) + VRAM_BASE);
+    dma_fill32(3, 0xA0, 0x30002a0, 0x400);
+    dma_fill32(3, 0xA0, 0x7000000, 0x400);
+    ValidateGameVersion();
+    write16(REG_BG0CNT, UNK_7E44.v18 | UNK_7E44.v22 | (UNK_7E44.v17 << 8) | (UNK_7E44.v16 << 2));
+    write16(REG_BG2CNT, UNK_7E44.v2 | UNK_7E44.v6 | (tmp3 << 8) | (UNK_7E44.v0 << 2));
+    write16(REG_BG3CNT, UNK_7E44.v10 | UNK_7E44.v14 | (tmp << 8) | (tmp2 << 2));
+    gUnk_6b8 = 0x5500;
+    gMainGameMode = 0;
+    gUnk_88 = 0;
+    gUnk_6c = 0xfe;
+    gUnk_70 = 1;
+    write16(REG_IE, 0x2001);
+    write16(REG_DISPSTAT, 8);
+    write16(REG_IF, 0xffff);
+    write16(REG_IME, 1);
+    gUnk_8c = tmp4 = 0x10;
+    gUnk_78 = 0;
+    gUnk_80 = 0x10;
+    write16(REG_BLDY, tmp4);
+    write16(REG_BLDALPHA, tmp4 << 8);
+    write16(REG_BLDCNT, 0x2844);
+    gUnk_24 = UNK_7E44.v12;
+}
