51d19d2ea26e4f687cc6f8f2d7979e5dcb941c68
biosp4rk <37962487+biosp4rk@users.noreply.github.com>
2025-09-17 17:36:57 -0700

    Update oam defines/macros

--- include/oam.h
+++ include/oam.h
@@ -12,8 +12,6 @@
 #define OBJ_MODE_OBJ_WINDOW       (1 << 11)
 #define OBJ_MOSAIC                (1 << 12)
 #define OBJ_COLOR_256_1           (1 << 13)
-#define OBJ_SHAPE_HORIZONTAL      (1 << 14)
-#define OBJ_SHAPE_VERTICAL        (1 << 15)
 
 #define OAM_AFFINE_MODE_NORMAL            0
 #define OAM_AFFINE_MODE_AFFINE            1
@@ -30,17 +28,10 @@
 
 // Attribute 1
 
-#define OBJ_X_FLIP     (1 << 12)
-#define OBJ_Y_FLIP     (1 << 13)
-#define OBJ_SIZE_16x16 (1 << 14)
-#define OBJ_SIZE_32x8  (1 << 14)
-#define OBJ_SIZE_8x32  (1 << 14)
-#define OBJ_SIZE_32x32 (1 << 15)
-#define OBJ_SIZE_32x16 (1 << 15)
-#define OBJ_SIZE_16x32 (1 << 15)
-#define OBJ_SIZE_64x64 ((1 << 15) | (1 << 14))
-#define OBJ_SIZE_64x32 ((1 << 15) | (1 << 14))
-#define OBJ_SIZE_32x64 ((1 << 15) | (1 << 14))
+#define OAM_NO_FLIP 0
+#define OAM_X_FLIP  (1 << 0)
+#define OAM_Y_FLIP  (1 << 1)
+#define OAM_XY_FLIP (OAM_X_FLIP | OAM_Y_FLIP)
 
 #define OAM_SIZE_S_8x8   0
 #define OAM_SIZE_S_16x16 1
@@ -57,9 +48,50 @@
 #define OAM_SIZE_V_16x32 2
 #define OAM_SIZE_V_32x64 3
 
-// Attribute 2
+#define OAM_DIMS_8x8   ((OAM_SHAPE_SQUARE << 2)     | OAM_SIZE_S_8x8)
+#define OAM_DIMS_16x16 ((OAM_SHAPE_SQUARE << 2)     | OAM_SIZE_S_16x16)
+#define OAM_DIMS_32x32 ((OAM_SHAPE_SQUARE << 2)     | OAM_SIZE_S_32x32)
+#define OAM_DIMS_64x64 ((OAM_SHAPE_SQUARE << 2)     | OAM_SIZE_S_64x64)
 
-#define OBJ_SPRITE_OAM 0x8000 // Not sure what this is, sprite OAM doesn't work without it
+#define OAM_DIMS_16x8  ((OAM_SHAPE_HORIZONTAL << 2) | OAM_SIZE_H_16x8)
+#define OAM_DIMS_32x8  ((OAM_SHAPE_HORIZONTAL << 2) | OAM_SIZE_H_32x8)
+#define OAM_DIMS_32x16 ((OAM_SHAPE_HORIZONTAL << 2) | OAM_SIZE_H_32x16)
+#define OAM_DIMS_64x32 ((OAM_SHAPE_HORIZONTAL << 2) | OAM_SIZE_H_64x32)
+
+#define OAM_DIMS_8x16  ((OAM_SHAPE_VERTICAL << 2)   | OAM_SIZE_V_8x16)
+#define OAM_DIMS_8x32  ((OAM_SHAPE_VERTICAL << 2)   | OAM_SIZE_V_8x32)
+#define OAM_DIMS_16x32 ((OAM_SHAPE_VERTICAL << 2)   | OAM_SIZE_V_16x32)
+#define OAM_DIMS_32x64 ((OAM_SHAPE_VERTICAL << 2)   | OAM_SIZE_V_32x64)
+
+#define OAM_DIMS_SIZE(dims) (dims & 3)
+#define OAM_DIMS_SHAPE(dims) ((dims >> 2) & 3)
+
+/**
+ * @brief A macro for creating OamData entries.
+ * Assumes that:
+ *   Rotation/Scaling = Off,
+ *   Double-Size / Disable = Normal,
+ *   OBJ Mode = Normal
+ *   Mosaic = Off,
+ *   Colors/Palettes = 16/16
+ */
+#define OAM_ENTRY(x, y, dims, flip, tile, palette, priority) \
+    OAM_DIMS_SHAPE(dims) << 14 | ((y) & 0xFF), \
+    OAM_DIMS_SIZE(dims) << 14 | (flip) << 12 | ((x) & 0x1FF), \
+    ((palette) << 12) | ((priority) << 10) | (tile)
+
+/**
+ * @brief A macro for creating OamData entries that allows specifying OBJ Mode.
+ * Assumes that:
+ *   Rotation/Scaling = Off,
+ *   Double-Size / Disable = Normal,
+ *   Mosaic = Off,
+ *   Colors/Palettes = 16/16
+ */
+#define OAM_ENTRY_MODE(x, y, dims, flip, tile, palette, priority, mode) \
+    OAM_DIMS_SHAPE(dims) << 14 | (mode) << 10 | ((y) & 0xFF), \
+    OAM_DIMS_SIZE(dims) << 14 | (flip) << 12 | ((x) & 0x1FF), \
+    ((palette) << 12) | ((priority) << 10) | (tile)
 
 struct FrameData {
     const u16* pFrame;
